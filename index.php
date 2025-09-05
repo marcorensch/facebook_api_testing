385ecdf789eb7373d0c88fd85cee36d587938580
Marco Rensch <marco.rensch@nx-designs.ch>
2025-09-05 12:54:23 +0200

    added readme.md

--- index.php
+++ index.php
@@ -5,7 +5,21 @@
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Meine Webseite</title>
+        <title>Facebook API Proof of Concept</title>
+        <style>
+            label {
+                font-weight: bold;
+                display: block;
+                margin-top: 5px;
+            }
+            textarea {
+                min-width: 300px;
+                height: 100px;
+            }
+            form {
+                margin-bottom: 40px;
+            }
+        </style>
     </head>
     <body>
         <header>
@@ -16,9 +30,18 @@
 
             <form action="process.php" method="POST" target="_blank">
             <fieldset>
+                <div>
+                    <label for="app_id">App ID (1)</label>
                     <input type="text" name="app_id" placeholder="App ID">
+                </div>
+                <div>
+                    <label for="app_secret">App Secret (2)</label>
                     <textarea name="app_secret" placeholder="App Secret"></textarea>
+                </div>
+                <div>
+                    <label for="short_user_token">Short Lived Token (3)</label>
                     <textarea name="short_user_token" placeholder="Short User Token"></textarea>
+                </div>
                 </fieldset>
                 <div>
                     <button type="submit">Submit</button>
@@ -27,8 +50,14 @@
 
             <form action="getPosts.php" method="POST" target="_blank">
                 <fieldset>
-                    <input name="page_id" placeholder="Page ID" />
-                    <textarea name="access_token" placeholder="Access Token"></textarea>
+                    <div>
+                        <label for="page_id">Page ID (4)</label>
+                        <input name="page_id" placeholder="Page ID" />
+                    </div>
+                    <div>
+                        <label for="access_token">Access Token (Long Lived) (5)</label>
+                        <textarea name="access_token" placeholder="Access Token"></textarea>
+                    </div>
                 </fieldset>
                 <div>
                     <button type="submit">Submit</button>
---
+++ readme.md
@@ -0,0 +1,40 @@
+# Facebook API Request Proof of Concept
+
+This is a very simple version for testing Facebook API requests.
+
+## Preparations
+
+### 1. Create an app
+
+1. Open [Facebook Developer Console](https://developers.facebook.com/?utm_source=chatgpt.com)
+2. Go to "My Apps"
+3. Create a new app 
+   - Select Miscellaneous (or other) as __Usecase__ in step 2 
+   - Select __Business__ as App Type
+
+Now you have an __app ID__ and __app secret__.
+
+### 2. Get an access token (Short lived)
+
+1. Open [Graph API Console](https://developers.facebook.com/tools/explorer/?utm_source=chatgpt.com)
+2. Select your __previously created app__ as  _Meta-App_
+3. Select __User Access Token__ as _User or page_
+5. Add the following permissions as _Authorisations_ (_Permissions_):
+   - `pages_show_list`
+   - `pages_read_engagement`
+6. Click __Generate Access Token__
+
+Now you have an short lived __access token__ for your app which you can use in the app.
+
+## Usage
+
+1. Open `index.php`
+2. Fill in the __app ID__, __app secret__, __short lived token__ and click __Submit__ in the first section.
+3. In a new window it shows you then a list of all pages you have access to including __page_id__. Aswell as the __access token__ for each page.
+4. Fill in the __ID__ (4), __Page Access Token (never-expire)__ (5) and click __Submit__ in the second section.
+5. In a new window it shows you the __posts__ of the page.
+
+note:::
+The __access token__ for the page is __never-expire__ and you can use it for all requests.
+This code was successfully tested with graph api version 19.0 & 23.0
+
